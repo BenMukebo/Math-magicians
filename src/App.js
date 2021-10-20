b90a035998f596edda4de44421b21968db0f020c
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-20 12:20:44 +0200

    Import Calculator inside the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import React, { Component } from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-function App() {
-  return (
-  );
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
