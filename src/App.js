fdee72cea00253fa39e23f3f94fa9c704525119b
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-22 12:11:16 +0200

    Refactor the App component from a class component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
 import './css/App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <main className="container">
-        <Calculator />
-      </main>
-    );
-  }
-}
+const App = () => (
+  <main className="container">
+    <Calculator />
+  </main>
+);
 
 export default App;
