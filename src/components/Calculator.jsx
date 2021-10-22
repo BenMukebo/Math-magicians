47902ea5eb451cb5921419706ddc74ed80e19952
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-22 07:04:16 +0200

    render method of the calculator.js

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -16,47 +16,80 @@ class Calculator extends Component {
   render() {
     const { total, next, operation } = this.state;
     return (
-      <ul className="items-container">
-        <li className="flex-center result">
-          <div className="input">
+      <section className="items-container">
+        <div className="result">
+          <div className=" flex-center input">
             {`
           ${total || operation || next
               ? `${total || ''} ${operation || ''} ${next || ''}`
               : '0'}
             `}
           </div>
-        </li>
-        <li className="flex-center keyItem">
-          <button className="btn" onClick={this.handleCalculate} type="button">AC</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">+/-</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">&#x25;</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">&#xf7;</button>
-        </li>
-        <li className="flex-center keyItem">
-          <button className="btn" onClick={this.handleCalculate} type="button">7</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">8</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">9</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">x</button>
-        </li>
-        <li className="flex-center keyItem">
-          <button className="btn" onClick={this.handleCalculate} type="button">4</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">5</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">6</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">-</button>
-        </li>
-        <li className="flex-center keyItem">
-          <button className="btn" onClick={this.handleCalculate} type="button">1</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">2</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">3</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">+</button>
-        </li>
-        <li className="flex-center keyItem">
-          <button className="btn" onClick={this.handleCalculate} type="button">0</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">.</button>
-          <button className="btn" onClick={this.handleCalculate} type="button">=</button>
-        </li>
-      </ul>
+        </div>
+        <ul className="items-container">
+          <li className="flex-center keyItem">
+            <button className="btn" onClick={this.handleCalculate} type="button">AC</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">+/-</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">&#x25;</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">&#xf7;</button>
+          </li>
+          <li className="flex-center keyItem">
+            <button className="btn" onClick={this.handleCalculate} type="button">7</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">8</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">9</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">x</button>
+          </li>
+          <li className="flex-center keyItem">
+            <button className="btn" onClick={this.handleCalculate} type="button">4</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">5</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">6</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">-</button>
+          </li>
+          <li className="flex-center keyItem">
+            <button className="btn" onClick={this.handleCalculate} type="button">1</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">2</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">3</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">+</button>
+          </li>
+          <li className="flex-center keyItem">
+            <button className="btn" onClick={this.handleCalculate} type="button">0</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">.</button>
+            <button className="btn" onClick={this.handleCalculate} type="button">=</button>
+          </li>
+        </ul>
+      </section>
     );
   }
 }
 export default Calculator;
+
+// const calculatorData = [
+//   [
+//     { className: 'btn', name: '+/-' },
+//     { className: 'btn', name: '%' },
+//     { className: 'btn', name: '÷' },
+//   ],
+//   [
+//     { className: 'btn', name: '7' },
+//     { className: 'btn', name: '8' },
+//     { className: 'btn', name: '9' },
+//     { className: 'btn', name: 'x' },
+//   ],
+//   [
+//     { className: 'btn', name: '4' },
+//     { className: 'btn', name: '5' },
+//     { className: 'btn', name: '6' },
+//     { className: 'btn', name: '-' },
+//   ],
+//   [
+//     { className: 'btn', name: '1' },
+//     { className: 'btn', name: '2' },
+//     { className: 'btn', name: '3' },
+//     { className: 'btn', name: '+' },
+//   ],
+//   [
+//     { className: 'btn', name: '0' },
+//     { className: 'btn', name: '.' },
+//     { className: 'btn', name: '=' },
+//   ],
+// ];
