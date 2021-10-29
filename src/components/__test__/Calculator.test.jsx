2eb180ea4c06d575c5dab11345dfcdf689f5bf6b
Ahmet <ahmt9417@gmail.com>
2021-10-29 14:14:16 +0300

    Fix test error

--- src/components/__test__/Calculator.test.jsx
+++ src/components/__test__/Calculator.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, screen, fireEvent } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
 
 import Calculator from '../Calculator';
 
@@ -14,12 +13,10 @@ describe('Calculator page component', () => {
   it('should return the sum of two number form UI', () => {
     render(<Calculator />);
     fireEvent.click(screen.getByText('3'));
-    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('x'));
     fireEvent.click(screen.getByText('7'));
     fireEvent.click(screen.getByText('='));
-    const result = screen.getByRole('result');
-    // screen.debug();
-    // expect(result.innerHTML).toBe('11');
-    expect(result).toHaveValue('11');
+    const result = screen.getByRole("none");
+    expect(result.innerHTML).toBe('21');
   });
 });
