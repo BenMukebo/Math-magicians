f9b4b6d0d7a8553cc8ef3ce27db80df6d94e351d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-29 13:56:05 +0200

    Remove all commented-out code

--- src/components/__test__/Quote.test.js
+++ src/components/__test__/Quote.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import isTSAnyKeyword from '@babel/types';
-// import { render } from '@testing-library/react';
 import Quote from '../Quote';
 
 it('matches snapshot', () => {
