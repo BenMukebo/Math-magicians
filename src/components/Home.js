8b03a6f71bf06dec2c6d03d1064c804c167bd314
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-31 12:03:08 +0200

    Add some superficial style

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,15 +13,9 @@ const Home = () => (
         computations that are involved in everyday life, but provided humans with the ability
         to understand mathematics on a greater scale than ever imagined.
       </p>
-      <p>
+      <p className="show">
         This technology allows students solve complicated problems quickly and in an efficient
-        manner.
-        <span className="show">
-          Students can develop fluency and mental math skills without a calculator.
-          Visual models and number sentences help students find answers while also developing
-          number sense, mental math, and problem solving abilities. When students become reliant
-          on calculators, their fluency and number sense actually decline over time.
-        </span>
+        manner Students can develop fluency and mental math skills without a calculator.
       </p>
     </div>
     <button className="btn-more" type="button">Read More</button>
